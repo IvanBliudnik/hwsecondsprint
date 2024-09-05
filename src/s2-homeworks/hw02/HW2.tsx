d02b4aae918b0afd2a69693d89c04d034069a9d1
IvanBliudnik <Bliudnik007@gmail.com>
2024-09-05 14:25:17 +0300

    secondcommit

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -2,12 +2,17 @@ import React, {useState} from 'react'
 import Affairs from './affairs/Affairs'
 import s2 from '../../s1-main/App.module.css'
 
-/*
-* 9 - в файле Affair.tsx дописать типизацию пропсов
-* 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
-* 11 - в файле Affair.tsx отобразить приходящие данные
-* */
-
+// * 1 - описать типы AffairPriorityType, AffairType;
+// * 2 - указать нужный тип для defaultAffairs;
+// * 3 - дописать типы и логику функции filterAffairs и проверить её тестами;
+// * 4 - выполнить пункт 3 для функции deleteAffair;
+// * 5 - указать нужный тип в useState с affairs;
+// * 6 - дописать тип и логику функции deleteAffairCallback;
+// * 7 - в файле Affairs.tsx дописать типизацию пропсов;
+// * 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow;
+// * 9 - в файле Affair.tsx дописать типизацию пропсов;
+// * 10 - в файле Affair.tsx дописать функции deleteCallback и использовать её;
+// * 11 - в файле Affair.tsx отобразить приходящие данные.
 // types
 export type AffairPriorityType = 'low' | 'middle' | 'high'
 export type AffairType = {
